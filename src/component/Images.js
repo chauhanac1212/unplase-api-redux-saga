ffd82ba29bd9ef6029f07312a42d2daf874ebea3
abhishek <abhishekkumar@appscrip.co>
2022-11-03 13:02:42 +0530

    first commit

--- src/component/Images.js
+++ src/component/Images.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Images = (props) => {
   return (
-    <div className="col-md-4 mt-3 col-lg-4">
+    <div className="col-md-4 col-lg-3 col-sm-6">
       <img
         className="img-fluid"
         src={props.img}
         alt="image"
         style={{
           width: "300px",
-          height: "200px",
+          height: "280px",
           objectFit: "cover",
-          marginBottom: "15px",
+          // marginBottom: "15px",
         }}
       />
     </div>
